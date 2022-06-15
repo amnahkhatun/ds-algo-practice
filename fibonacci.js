7ef8dc0a55f70f92a0c27766e0be312062f222ae
amnahkhatun <amna.khatun1605@gmail.com>
2022-06-15 11:19:36 +0530

    practice

--- fibonacci.js
+++ fibonacci.js
@@ -1,3 +1,6 @@
+let fibCalculations = 0;
+let memoCalculations = 0;
+
 fibonacciIterative = num => {
   let n1 = 0;
   let n2 = 1;
@@ -21,10 +24,44 @@ fibonacciRecursive = num => {
 };
 
 fibonacciRecursive1 = num => {
-  if (num == 2 || num == 1) {
-    return 1;
+  fibCalculations ++;
+  if (num < 2) {
+    return num;
   }
   return fibonacciRecursive(num - 1) + fibonacciRecursive(num - 2);
 };
 // console.log(fibonacciRecursive(9000));
-console.log(fibonacciRecursive1(9000));
+
+//Recursion using Memoization
+
+function fibMemo() {
+  let cache = {};
+  return function fib(n) {
+    memoCalculations++;
+    if (n in cache) {
+      return cache[n]
+    } else {
+      if (n < 2) {
+        return n
+      } else {
+        cache[n] = fib(n - 1) + fib(n - 2)
+        return cache[n]
+      }
+    }
+  }
+}
+
+function fibDP2(n){
+  let answer = [0,1];
+  for(let i=2;i<=n;i++){
+    answer.push(answer[i-2] + answer[i-1])
+  }
+  return answer.pop()
+}
+console.log(fibonacciRecursive1(100));
+console.log(fibMemo()(100))
+console.log(fibDP2(100));
+
+
+// console.log('slow', fibCalculations)
+// console.log('fast', memoCalculations)
